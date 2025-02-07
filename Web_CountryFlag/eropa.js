af52fb67483ef9380245dc0aa5b1c25796ae79cd
KenMajas <139088915+KenshiHimura29@users.noreply.github.com>
2025-02-07 10:31:27 +0700

    Add files via upload

---
+++ Web_CountryFlag/eropa.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    let container = document.getElementById("container");
+    $.ajax({
+        url: 'https://restcountries.com/v3.1/region/europe',
+        type: 'GET',
+        success: function(response) {
+            const sortedRes = response.sort((a, b) => {
+                return a.name.common.localeCompare(b.name.common);
+            });
+
+            sortedRes.map((data) => {
+                container.innerHTML += `
+                <div class="box">
+                    <a href="detail-negara.html?country=${data.name.common}">
+                        <img src="${data.flags.png}" alt="Flag of ${data.name.common}"/>
+                    </a>
+                    <h2>${data.name.common}</h2>
+                    <h4>Continent: ${data.continents}</h4>
+                    <h4>Official: ${data.name.official}</h4>
+                    <h4>Capital: ${data.capital}</h4>
+                    <h4>Population: ${data.population}</h4>
+                </div>
+                `;
+            });
+        }
+    });
+});
